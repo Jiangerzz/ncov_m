f36beac8468136b427235df798f1093994b8ddae
Jiang <1347477532@qq.com>
2023-03-12 21:49:29 +0800

    "初步创建前端项目"

---
+++ src/views/ECharts_ncov.vue
@@ -0,0 +1,117 @@
+<template>
+    <div ref="echarts_box" class="content"></div>
+</template>
+
+<script>
+
+import * as echarts from 'echarts'
+import jsonp from 'jsonp'
+
+const option = {
+  title: {
+    text: "中国疫情地图",
+    link: 'https://www.baidu.com',
+    subtext: "Jiang",
+    sublink: "https://www.baidu.com"
+  },
+  series: [{
+    name: '确诊人数',
+    type: 'map',
+    map: 'china',
+    label: {
+      show: true,
+      color: '#ffffff',
+      fontSize: 8
+    },
+    itemStyle: {
+      areaColor: '#eee'
+    },
+    roam: true,
+    zoom: 1.2,
+    emphasis: {
+      label: {
+        color: '#fff',
+        fontSize: 12,
+      },
+      itemStyle: {
+        areaColor: '#83b5e7'
+      }
+    },
+    data: []
+  }],
+  // {name: xxx, value: xxx}
+  visualMap: [{
+    type: 'piecewise',
+    show: true,
+    pieces: [
+      { min: 10000 },
+      { min: 1000, max: 9999 },
+      { min: 100, max: 999},
+      { min: 10, max: 99 },
+      { min: 1, max: 9 }
+    ],
+    // align: 'right'
+    // showLabel: false
+    inRange: {
+      symbol: 'rect',
+      color: [
+        '#ffc0b1',
+        '#9c0505',
+      ]
+    },
+    itemWidth: 20
+  }],
+  tooltip: {
+    trigger: 'item'
+  },
+  toolbox: {
+    show: true,
+    orient: 'vertical',
+    left: 'right',
+    top: 'center',
+    feature: {
+      dataView: { readOnly: false },
+      restore: {},
+      saveAsImage: {}
+    }
+  }
+}
+
+export default {
+  name: "ECharts_ncov",
+  methods: {
+    getData () {
+      jsonp('https://interface.sina.cn/news/wap/fymap2020_data.d.json?_=1580892522427', {}, (err, data) => {
+        if (!err) {
+          // eslint-disable-next-line no-console
+          //console.log(data)
+          let list = data.data.list.map(item => (
+              
+              {
+                name: item.name,
+                value: item.value,
+                susNum: item.susNum
+              }))
+          option.series[0].data = list
+          this.mycharts.setOption(option)
+        }
+      })
+    }
+  },
+  mounted() {
+    this.getData()
+    this.mycharts = echarts.init(this.$refs.echarts_box)
+    this.mycharts.setOption(option)
+  }
+  
+}
+
+</script>
+
+<style scoped>
+.content {
+  width: 1500px;
+  height: 638px;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="echarts_box" style="width: 600px;height:400px;"></div>
 </template>
 
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import * as echarts from 'echarts'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+
+  created() {},
+  // DOM 渲染完成触发
+  mounted() {
+    // 1. 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('echarts_box'))
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
   }
 }
 </script>
